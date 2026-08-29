e218a99d8f3ea1cda49e75b3349791ef1824985f
王恒 <1066026709@qq.com>
2026-08-29 13:42:04 +0800

    解决编译问题

---
+++ AWHBBasicBusiness.framework/Headers/AWHBBBFontChangeView.h
@@ -0,0 +1,19 @@
+//
+//  AWHBBBFontChangeView.h
+//  AWHBBasicBusiness
+//
+//  Created by 王恒 on 2025/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AWHBBBFontChangeView : UIView
+
+/// 刷新UI
+- (void)refreshUI;
+
+@end
+
+NS_ASSUME_NONNULL_END
